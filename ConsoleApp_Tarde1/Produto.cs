6fcbf073b92cfae6634696039c2393e108cbe13a
Gabriel Lima Marcondes <gabriellimamarcondes34@gamil.com>
2023-03-08 15:00:32 -0300

    criei os metodos get da classe produtos

--- ConsoleApp_Tarde1/Produto.cs
+++ ConsoleApp_Tarde1/Produto.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp_Tarde1
 {
-    internal class Produto
+   public class Produto
     {
+        private int Id;
+        private string Descricao;
+        private decimal valor;
+            
+        public Produto(int id, string descricao, decimal valor)
+        {
+            Id = id;
+            Descricao = descricao;
+            Valor = valor;
+
+
+        }
+        public int getId()
+        { return Id; }
+        public string getDescricao()
+        { return Descricao; }
+        public decimal getValor() 
+        {  return valor; }
     }
 }
